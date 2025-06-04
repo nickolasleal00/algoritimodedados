3c2285d076e8a42700194edcdbddba9dae46cf7d
Nickolas Leal <nickolasleal6@gmail.com>
2025-06-04 09:38:57 -0300

    Delete aula02 directory

--- aula02/Cidade.py
+++
@@ -1,7 +0,0 @@
-class Cidade:
-    def __init__(self, name = "Tangamandápio"): 
-        self.id = None
-        self.nome = name
-
-    def __str__(self):
-        return "Id: " + str(self.id) + " ------ " + "Nome: " + self.nome    
