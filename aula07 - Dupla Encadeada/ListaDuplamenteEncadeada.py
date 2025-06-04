c8dde3e23d17a1dc895e9cc8494005e9738f8521
Nickolas Leal Martins <nickolasleal6@gmail.com>
2025-06-04 11:23:33 -0300

    Lista Duplamente Encadeada

---
+++ aula07 - Dupla Encadeada/ListaDuplamenteEncadeada.py
@@ -0,0 +1,88 @@
+from No import No
+class ListaDuplamenteEncadeada:
+	def __init__(self):
+		self.inicio = None
+		self.fim = None
+
+	def add(self, valor):
+		nodo = No(valor)
+		if self.inicio == None:
+			self.inicio = nodo
+			self.fim = nodo
+		else:
+			if nodo.dado < self.inicio.dado:
+				nodo.proximo = self.inicio
+				self.inicio.anterior = nodo
+				self.inicio = nodo
+			else:
+				ant = self.inicio
+				aux = self.inicio.proximo
+				while aux:
+					if nodo.dado < aux.dado:
+						nodo.proximo = ant.proximo 
+						ant.proximo = nodo
+						nodo.anterior = ant 
+						aux.anterior = nodo
+						break
+					else:
+						ant = aux
+						aux = aux.proximo
+				if aux == None and nodo.dado >= ant.dado:
+					ant.proximo = nodo
+					nodo.anterior = ant
+					self.fim = nodo
+		self.imprimir()
+
+	def imprimir(self):
+		print("------Lista Duplamente Encadeada-----------------")
+		if self.inicio == None:
+			print( "Lista Vazia" )
+		else:
+			aux = self.inicio
+			while aux:
+				print( aux.dado )
+				aux = aux.proximo
+
+	def imprimirReverso(self):
+		print("------Lista Duplamente Encadeada Reversa-----------------")
+		if self.inicio == None:
+			print( "Lista Vazia" )
+		else:
+			aux = self.fim
+			while aux:
+				print( aux.dado )
+				aux = aux.anterior
+
+	def remover(self, valor):
+		encontrou = False
+		if self.inicio != None:
+
+		
+			if self.inicio.dado == valor:
+				encontrou = True
+				self.inicio = self.inicio.proximo
+				if self.inicio == None:
+					self.fim = None
+				else: 
+					self.inicio.anterior = None
+				return
+
+			
+			else:
+				ant = self.inicio
+				aux = self.inicio.proximo
+				while aux:
+					if aux.dado == valor:
+						encontrou = True
+						if aux.proximo == None:
+							self.fim = ant
+						else:
+							aux.proximo.anterior = ant
+						ant.proximo = aux.proximo
+						break
+					else:
+						ant = aux
+						aux = aux.proximo
+			if not encontrou:
+				print( "Valor não encontrado")
+		self.imprimir()
---
+++ aula07 - Dupla Encadeada/main.py
@@ -0,0 +1,16 @@
+from ListaDuplamenteEncadeada import ListaDuplamenteEncadeada
+
+lista = ListaDuplamenteEncadeada()
+
+lista.imprimir()
+
+lista.add("Roberto")
+lista.add("Henrique")
+lista.add("Estela")
+lista.add("Gabriel")
+
+lista.remover("Adalto")
+lista.remover("João")
+lista.remover("Maria")
+
+lista.imprimirReverso()
