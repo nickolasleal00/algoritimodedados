8248f01be0f8acb5fd5ff7539fb93a3dd6dc2bfa
Nickolas Leal Martins <nickolasleal6@gmail.com>
2025-06-04 10:15:43 -0300

    Lista Encadeada

---
+++ aula06 - Lista Encadeada/ListaEncadeada.py
@@ -0,0 +1,42 @@
+from No import No
+
+class ListaEncadeada:
+    def __init__(self):
+        self.inicio = None
+
+    def addNoInicio(self, valor):
+      nodo = No(valor)  
+      if self.inicio is None:
+         self.inicio = nodo
+      else:
+          if self.inicio.prox is None:
+             self.inicio.prox = nodo
+          
+            
+    def addNoFim(self, valor):
+      nodo = No(valor)  
+      if self.inicio is None:
+         self.inicio = nodo
+      else:
+          if self.inicio.prox is None:
+             self.inicio.prox = nodo
+          else:
+             aux = self.inicio.prox
+             while aux.prox is not None:
+                 aux = aux.prox
+             aux.prox = nodo
+      self.imprimir()                          
+  
+    def imprimir(self):
+       print("---------------------------------")
+       if self.inicio is None:
+          print("Lista encadeada está vazia!")
+       else:
+          aux = self.inicio
+          while aux:
+               print(aux.dado)
+               aux = aux.prox
+
+
+    
+
---
+++ aula06 - Lista Encadeada/main.py
@@ -0,0 +1,7 @@
+from ListaEncadeada import ListaEncadeada
+
+lista = ListaEncadeada()
+lista .imprimir()
+lista.addNoInicio("Adalto")
+lista.addNoFim("Abel")
+lista.addNoFim("Cleber")
